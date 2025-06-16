1902a8a50906b6f2037cd44feeaeb7650634bcd3
Moritz Stückler <moritz@bitbetter.de>
2025-06-16 08:33:07 +0200

    fix: readme, config, favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import db from '@astrojs/db';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), db()],
   output: "server",
-
+  site: "https://unfallkarte-hamburg.de",
   vite: {
     plugins: [tailwindcss()]
   },
